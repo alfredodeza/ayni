d6ac822f6f7a90a79e4b3643291b24bc1a1c5b7b
Alfredo Deza <alfredo@deza.pe>
2014-08-29 16:17:24 -0400

    update the project model

--- ayni/models/projects.py
+++ ayni/models/projects.py
@@ -1,8 +1,22 @@
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 from ayni.models import Base
 
 
 class Project(Base):
 
-    __tablename__ = 'jobs'
+    __tablename__ = 'projects'
     id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False, unique=True, index=True)
+    fqdn = Column(String(256))
+
+    docs = relationship('Doc',
+                        backref=backref('project'),
+                        cascade='all,delete',
+                        lazy='dynamic',
+                        order_by='Doc.doc_id',
+                        )
+
+    def __init__(self, name, fqdn):
+        self.name = name
+        self.fqdn = fqdn
