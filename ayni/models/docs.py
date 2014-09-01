0fb2ae81b49691eace8fcdd6f8e3817f0a880eb7
Alfredo Deza <alfredo@deza.pe>
2014-09-01 17:04:39 -0400

    provide full url and end url for docs

--- ayni/models/docs.py
+++ ayni/models/docs.py
@@ -1,5 +1,7 @@
+import os
 from sqlalchemy import Column, Integer, String, ForeignKey
 from ayni.models import Base
+from ayni.util import normalize_fqdn
 
 
 class Doc(Base):
@@ -15,9 +17,25 @@ class Doc(Base):
         self.project = project
         self.version = version
         self.url_prefix = url_prefix
-        self.fqdn = fqdn or project.fqdn
+        self.fqdn = normalize_fqdn(fqdn) or project.fqdn
 
     @property
-    def url(self):
-        return "/%s/%s/" % (self.url_prefix, self.version)
+    def full_url(self):
+        url = "%s%s" % (self.fqdn, self.end_url)
+        return url
 
+    @property
+    def end_url(self):
+        url = os.path.join(self.url_prefix, self.version)
+        if not url.endswith('/'):
+            return url + '/'
+        return url
+
+    def __json__(self):
+        return dict(
+            version=self.version,
+            url_prefix=self.url_prefix,
+            fqdn=self.fqdn,
+            end_url=self.end_url,
+            full_url=self.full_url
+        )
