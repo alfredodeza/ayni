a397cdedf3a056259ae728aebb0ca915b4dd6c83
Alfredo Deza <alfredo@deza.pe>
2014-09-02 14:21:59 -0400

    fix tests with proper full_url s

--- ayni/tests/models/test_docs.py
+++ ayni/tests/models/test_docs.py
@@ -18,7 +18,7 @@ class TestDocsController(object):
 
     def test_project_has_single_docs(self, session):
         p = Project('foobar', 'example.com')
-        Doc(p, 'stable', '/docs/')
+        Doc(p, 'stable', 'latest', '/docs/')
         session.commit()
         result = session.app.get('/projects/foobar/docs/')
         assert result.status_int == 200
@@ -26,8 +26,8 @@ class TestDocsController(object):
 
     def test_project_has_multiple_docs(self, session):
         p = Project('foobar', 'example.com')
-        Doc(p, 'stable', '/docs/stable/')
-        Doc(p, 'development', '/docs/development/')
+        Doc(p, 'stable', 'latest', '/docs/stable/')
+        Doc(p, 'development', 'dev', '/docs/development/')
 
         session.commit()
         result = session.app.get('/projects/foobar/docs/')
@@ -39,19 +39,19 @@ class TestDocController(object):
 
     def test_single_doc_version(self, session):
         p = Project('foobar', 'example.com')
-        Doc(p, 'stable', '/docs/stable/')
-        Doc(p, 'development', '/docs/development/')
+        Doc(p, 'stable', 'latest', '/docs/')
+        Doc(p, 'development', 'dev', '/docs/')
         session.commit()
 
-        result = session.app.get('/projects/foobar/docs/stable/')
+        result = session.app.get('/projects/foobar/docs/latest/')
         assert result.status_int == 200
         assert result.json['fqdn'] == 'http://example.com'
-        assert result.json['version'] == 'stable'
-        assert result.json['url_prefix'] == '/docs/stable/'
+        assert result.json['version'] == 'latest'
+        assert result.json['url_prefix'] == '/docs/'
 
     def test_cannot_find_doc_version(self, session):
         p = Project('foobar', 'example.com')
-        Doc(p, 'development', '/docs/development/')
+        Doc(p, 'development', 'dev', '/docs/')
         session.commit()
 
         result = session.app.get('/projects/foobar/docs/stable/', expect_errors=True)
@@ -59,18 +59,34 @@ class TestDocController(object):
 
     def test_get_full_url(self, session):
         p = Project('foobar', 'example.com')
-        Doc(p, 'stable', '/docs/')
+        Doc(p, 'stable', 'latest', '/docs/')
         session.commit()
 
-        result = session.app.get('/projects/foobar/docs/stable/')
+        result = session.app.get('/projects/foobar/docs/latest/')
         assert result.status_int == 200
-        assert result.json['full_url'] == 'http://example.com/docs/stable/'
+        assert result.json['full_url'] == 'http://example.com/docs/latest/'
+
+    def test_get_fallback_redirect_to(self, session):
+        p = Project('foobar', 'example.com')
+        Doc(p, 'stable', 'latest', '/docs/')
+        session.commit()
+
+        result = session.app.get('/projects/foobar/docs/latest/')
+        assert result.json['redirect_to'] == 'http://example.com/docs/latest/'
 
     def test_get_end_url(self, session):
         p = Project('foobar', 'example.com')
-        Doc(p, 'stable', '/docs/')
+        Doc(p, 'stable', 'latest', '/docs/')
         session.commit()
 
-        result = session.app.get('/projects/foobar/docs/stable/')
+        result = session.app.get('/projects/foobar/docs/latest/')
         assert result.status_int == 200
-        assert result.json['end_url'] == '/docs/stable/'
+        assert result.json['end_url'] == '/docs/latest/'
+
+    def test_get_redirect_to_override(self, session):
+        p = Project('foobar', 'example.com')
+        Doc(p, 'stable', 'latest', '/docs/', redirect_to='http://ceph.com/docs/')
+        session.commit()
+
+        result = session.app.get('/projects/foobar/docs/latest/')
+        assert result.json['redirect_to'] == 'http://ceph.com/docs/'
