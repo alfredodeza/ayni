496a5c53dcd0d13f0a094b8c3501a8a816dd8db1
Alfredo Deza <alfredo@deza.pe>
2014-09-02 14:43:45 -0400

    fixed a problem in projects where get_docs was not searching for name

--- ayni/models/projects.py
+++ ayni/models/projects.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship, backref
 from ayni.models import Base
-from ayni.models.docs import Doc
 from ayni.util import normalize_fqdn
 
 
@@ -16,14 +15,14 @@ class Project(Base):
                 'Doc',
                 backref=backref('project'),
                 cascade='all,delete',
-                order_by='Doc.id',
+                order_by='asc(Doc.weight)',
            )
 
     def __init__(self, name, fqdn):
         self.name = name
         self.fqdn = normalize_fqdn(fqdn)
 
-    def get_doc(self, version):
+    def get_doc(self, name):
         for d in self.docs:
-            if d.version == version:
+            if d.name == name:
                 return d
